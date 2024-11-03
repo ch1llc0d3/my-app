aefb8e6e198801dc6a469867b2340044a394deea
ch1llc0d3 <chillcrib7@gmail.com>
2024-11-03 18:00:18 -0300

    fix

---
+++ src/components/router/index.js
@@ -0,0 +1,33 @@
+import Vue from 'vue';
+import Router from 'vue-router';
+import HomeView from '@/views/HomeView.vue'; // Importa el componente con el nombre corregido
+import AddTask from '@/views/AddTask.vue';
+import TaskList from '@/views/TaskList.vue';
+import CombinedView from '@/views/CombinedView.vue';
+
+Vue.use(Router);
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'HomeView', // Aquí debe coincidir con el nuevo nombre del componente
+      component: HomeView,
+    },
+    {
+      path: '/addtask',
+      name: 'AddTask',
+      component: AddTask
+    },
+    {
+      path: '/tasklist',
+      name: 'TaskList',
+      component: TaskList
+    },
+    {
+      path: '/combined',
+      name: 'CombinedView',
+      component: CombinedView
+    },
+  ]
+});
