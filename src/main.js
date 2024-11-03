0acae35de5c99226281337d363c6faf12bc633e1
ch1llc0d3 <chillcrib7@gmail.com>
2024-11-03 18:03:16 -0300

    adding main.js

--- src/main.js
+++ src/main.js
@@ -1,5 +1,14 @@
-import { createApp } from 'vue'
-import './style.css'
+import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
+// src/main.js
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
 
-createApp(App).mount('#app')
+
+Vue.config.productionTip = false
+
+new Vue({
+  router,
+  render: h => h(App)
+}).$mount('#app')
