1746b330d300b508bc66fc5564ce7ad2b230843b
ch1llc0d3 <chillcrib7@gmail.com>
2024-11-04 08:08:41 -0300

    vite.config

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import vue from '@vitejs/plugin-vue';
 import path from 'path';
 
 export default defineConfig({
-  base: 'my-app',
+  base: '/my-app/',
   plugins: [vue()],
   resolve: {
     alias: {
